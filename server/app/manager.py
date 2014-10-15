2337dbb22e52e46493c2eb5d9817a255f8137f32
7flying <diez.f.irene@gmail.com>
2014-10-15 23:44:04 +0200

    Manager's main methods finished

--- server/app/manager.py
+++ server/app/manager.py
@@ -10,6 +10,11 @@ KEY_DATE = 'date'
 KEY_TAGS = 'tags'
 # User-tag, post-tag set key
 APPEND_KEY_TAG = '-tags'
+# User-posts key
+APPEND_KEY_POSTS = '-posts'
+# Identifiers. This ids are used to reference the tags and the posts.
+POST_ID = 'key-post-id'
+TAG_ID = 'tag-id'
 
 def populate_test():
 	db.hset('post-1', 'title', 'How to fly')
@@ -64,7 +69,7 @@ def insert_user(username, password):
 	Inserts a user in the db. Returns False if there is alrealdy a user in the
 	db with that username.
 	"""
-	if !_is_user_created(username):
+	if not _is_user_created(username):
 		db.hset(username, KEY_PASSWORD, password)
 		return True
 	else:
@@ -75,14 +80,37 @@ def insert_tag_user_tags(username, tag):
 	Inserts a tag to the user's set of tags.
 	"""
 	# Since it is a set, the elements aren't inserted if present
-	db.ssad(username + APPEND_KEY_TAG, tag)
+	db.sadd(username + APPEND_KEY_TAG, tag)
+
+def delete_tag_user_tags(username, tag):
+	"""
+	Deletes a tag from the user's set of tags
+	and from the posts the user has writen.
+	"""
+	db.srem(user + APPEND_KEY_TAG, tag)
+	_delete_tag_from_all_user_posts(username, tag)
+
+def _delete_tag_from_all_user_posts(username, tag):
+	"""
+	Deletes a tag from all the posts a given user has.
+	"""
+	for post_id in db.smembers(username + APPEND_KEY_POSTS):
+		delete_tag_from_post(post_id)
 
 def insert_tag_post_tags(post_id, tag):
 	"""
 	Inserts a tag to the post's set of tags.
 	"""
 	# Element isn't inserted if present
-	db.ssad(post_id + APPEND_KEY_TAG, tag)
+	if _is_post_created(post_id):
+		db.sadd(get_post(post_id)[KEY_TAGS], tag)
+
+def delete_tag_from_post(post_id, tag):
+	"""
+	Deletes a tag from the post's set of tags
+	"""
+	if _is_post_created(post_id):
+		db.srem(get_post(post_id)[KEY_TAGS], tag)
 
 def _is_post_created(post_id):
 	"""
@@ -115,26 +143,54 @@ def get_post(key):
 	else:
 		return None
 
-def get_posts(user_id):
+def get_posts(username):
+	"""
+	Returns all the posts writen by a user.
+	"""
+	posts_ids = db.smembers(username + APPEND_KEY_POSTS)
 	posts = []
-	posts.append(get_post('post-1'))
-	posts.append(get_post('post-2'))
+	for key in posts_ids:
+		posts.append(get_post(key))
 	return posts
 
-def insert_post(post):
-	db.hset(post['key'], 'title', post['title'])
-	db.hset(post['key'], 'date', post['date'])
-	db.hset(post['key'], 'contents', post['contents'])
-	db.hset(post['key'], 'tags', post['tags'])
+def insert_post(post, username):
+	"""
+	Inserts a new post in the db.
+	"""
+	post_id = db.incr(POST_ID)
+	db.hset(post_id, KEY_TITLE, post[KEY_TITLE])
+	db.hset(post_id, KEY_DATE, post[KEY_DATE])
+	db.hset(post_id, KEY_CONTENTS, post[KEY_CONTENTS])
+	# Set a new set of tags
+	tags_id = db.incr(TAG_ID)
+	for tag in post[KEY_TAGS]: # TODO: See if can be made without the for
+		db.sadd(tags_id, tag)
+	db.hset(post_id, KEY_TAGS, tags_id)
+	# Add post id to users post-set
+	db.sadd(username + APPEND_KEY_POSTS, post_id)
 
-def update_post(post):
-	db.hset(post['key'], 'title', post['title'])
-	db.hset(post['key'], 'date', post['date'])
-	db.hset(post['key'], 'contents', post['contents'])
-	db.hset(post['key'], 'tags', post['tags'])
+def update_post(post, post_id, username):
+	"""
+	Updates a post in the db.
+	"""
+	db.hset(post_id, KEY_TITLE, post[KEY_TITLE])
+	db.hset(post_id, KEY_DATE, post[KEY_DATE])
+	db.hset(post_id, KEY_CONTENTS, post[KEY_CONTENTS])
+	tag_id = db.hget(post_id, KEY_TAGS)
+	# Add new tags (if any)
+	for tag in post[KEY_TAGS]:
+		db.sadd(tag_id, tag)
 
-def delete_post(id):
-	db.hdel(id, 'title')
-	db.hdel(id, 'date')
-	db.hdel(id, 'contents')
-	db.h(id, 'tags')
+def delete_post(post_id, username):
+	"""
+	Deletes a post (as well as its related set of tags).
+	"""
+	db.hdel(post_id, KEY_TITLE)
+	db.hdel(post_id, KEY_DATE)
+	db.hdel(post_id, KEY_CONTENTS)
+	# Delete the set of tags that the post has
+	tags_id = db.hget(post_id, KEY_TAGS)
+	db.delete(tags_id)
+	db.hdel(post_id, KEY_TAGS)
+	# Delete the post id from the user's post list
+	db.srem(username + APPEND_KEY_POSTS, post_id)
