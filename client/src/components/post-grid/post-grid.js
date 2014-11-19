85ab9275b43ec70bf1ea34adac449df091ca0f62
7flying <diez.f.irene@gmail.com>
2014-11-19 23:39:43 +0100

    proper structure + routing

--- client/src/components/post-grid/post-grid.js
+++ client/src/components/post-grid/post-grid.js
@@ -4,22 +4,65 @@ define(['knockout', 'text!./post-grid.html'], function(ko, templateMarkup) {
   	// Expose a 'posts' property to the view
   	this.posts = ko.observableArray();
   	//this.posts = [ {'title': "yo", 'author': "another"}, {title: "yo", author: "another"}]
-    //$.getJSON('http://localhost:5000/yilpil/updates?resource=posts', this.posts);
-    var params = {'resource' : 'posts'};
+    /* Deja la respuesta en response header */
+    /*
+    var res;
+    $.getJSON('http://localhost:5000/yilpil/updates?resource=posts&callback=?', function(data) {
+      res = data;
+      window.alert(res);
+    });
+    */
+     //var url = 'http://localhost:5000/yilpil/updates?resource=posts&callback=?';
+    /*
+    var sendData = function(url, method, data){
+      var request = {
+                    url: url,
+                    type: method,
+                    contentType: "application/json",
+                    accepts: "application/json",
+                    cache: false,
+                    dataType: 'json',
+                    data: JSON.stringify(data),
+                    error: function(jqXHR) {
+                        console.log("ajax error " + jqXHR.status);
+                    }
+                };
+                return $.ajax(request);
+      }
+    var response = sendData(url, 'GET').done(function(data) {
+      console.log(data);
+    });
+    console.log(response);
+    */
+    //$.getJSON(url, function(data){ console.log(data); });
+
+    /*
     $.ajax({
-        url: 'http://localhost:5000/yilpil/updates',
-        type: 'GET',
-        contentType: 'application/json',
-        data: "resource=posts",
-        dataType: 'json',
-        complete: function(data) {
-          window.alert(data);
-          //this.channels = data.posts;
-        },
-        error: function(xhr, ajaxOptions, thrownError) {
-          window.alert("some error");
-        }
-      });
+      type: 'GET',
+      url: url,
+      contentType: "application/json",
+      dataType: 'jsonp',
+      jsonpCallback: 'local'
+    });
+    function local(json) {
+      console.log(json);
+    }
+    */
+    /*
+    $.ajax({
+      url: 'http://localhost:5000/yilpil/updates?resource=posts&callback=?',
+      dataType: 'json',
+      jsonpCallback: function(back) {
+        alert("at back");
+        console.log(back);
+      }, // specify the callback name if you're hard-coding it
+      success: function(data){
+      // we make a successful JSONP call!
+        alert("at success");
+        console.log(data);   
+      }
+    });
+    */
     /*
     this.posts = [{
       title: "Post Num 12", author: "seven",
@@ -33,7 +76,12 @@ define(['knockout', 'text!./post-grid.html'], function(ko, templateMarkup) {
       id: "15",  
       votes: "0"
     }] */
-  }
+    var request = require(['request']);
+    request('http://www.google.com', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body) // Print the google web page.
+      }
+    })};
 
     
   return { viewModel: PostGrid, template: templateMarkup };
