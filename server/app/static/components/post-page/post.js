b53a9f6f58e494d6a6ed1ef57100e5e3fa46bc77
7flying <diez.f.irene@gmail.com>
2014-11-24 23:55:09 +0100

    try to render post

--- server/app/static/components/post-page/post.js
+++ server/app/static/components/post-page/post.js
@@ -3,19 +3,25 @@ define(['knockout', 'text!./post.html'], function(ko, template) {
 	function PostViewModel(params) {
 
 		this.post = ko.observable();
-
+		this.title = ko.observable();
+		this.potId = ko.observable(window.location.href.charAt(window.location.href.length));
 		ko.postbox.subscribe("idSender", function(receivedValue) {
 			//console.log("data received from postbox : " + receivedValue);
-			getPost(this.post, receivedValue);
+			this.postId = receivedValue;
+			//getPost(this.post, receivedValue);
 		}, PostViewModel);
 
-		var getPost = function(toStore, id) {
+		var getPost = function(toStore, id, sel) {
 			console.log("at get post")
 			$.getJSON('/yilpil/post/' + id, function(data) {
 				toStore = data;
-				console.log("data stored ");
+				sel = toStore['title'];
+				console.log(sel);
 			});
 		};
+
+		getPost(this.post, this.potId, this.title);
+		
 	}
 	return { viewModel: PostViewModel, template: template };
 });
