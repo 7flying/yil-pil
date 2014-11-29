60abc9f29d553be32cd9336ae5e640dbd270e7b4
flying <diez.f.irene@gmail.com>
2014-11-30 00:25:46 +0100

    Search gives warning when nothing is found

--- server/app/static/components/search-page/search.js
+++ server/app/static/components/search-page/search.js
@@ -9,10 +9,15 @@ define(['knockout', 'text!./search.html','module', 'app/router'], function(ko, t
 		var self = this;
 		this.query = ko.observable(params.query);
 		this.searchPosts = ko.observableArray();
+		this.results = ko.observable(null);
 
 		var getPosts = function(toStore) {
 			var url = '/yilpil/search/posts/title?title=' + self.query();
 			$.getJSON(url, function(data) {
+				if (data.length ==  0)
+					self.results(null);
+				else
+					self.results(data.length + " posts matched:");
 				while (data.length > 0) {
 					var temp = data.shift();
 					// Set preview
