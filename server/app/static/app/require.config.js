9c2b6c9cf9b0ee29f08ed08d4732f01a371ce3f7
flying <diez.f.irene@gmail.com>
2014-11-30 21:24:38 +0100

    filtering users' posts by date display and data check

--- server/app/static/app/require.config.js
+++ server/app/static/app/require.config.js
@@ -3,12 +3,16 @@ var require = {
     baseUrl: "./",
     paths: {
         "bootstrap":            "bower_modules/components-bootstrap/js/bootstrap.min",
+        "bootstrap-datetimepicker":
+                                "bower_modules/bootstrap-datetimepicker/src/js/bootstrap-datetimepicker",
         "crossroads":           "bower_modules/crossroads/dist/crossroads.min",
         "hasher":               "bower_modules/hasher/dist/js/hasher.min",
         "jquery":               "bower_modules/jquery/dist/jquery",
         "knockout":             "bower_modules/knockout/dist/knockout",
         "knockout-projections": "bower_modules/knockout-projections/dist/knockout-projections",
         "knockout-postbox":     "bower_modules/knockout-postbox/build/knockout-postbox",
+        "moment":               "bower_modules/moment/min/moment.min",
+        "request":              "bower_modules/request/request",
         "signals":              "bower_modules/js-signals/dist/signals.min",
         "text":                 "bower_modules/requirejs-text/text"
     },
--- server/app/static/components/user-page/user.js
+++ server/app/static/components/user-page/user.js
@@ -1,4 +1,4 @@
-define(['knockout', 'text!./user.html', 'module', 'app/router'], function(ko, template, module, router) {
+define(['knockout', 'text!./user.html', 'module', 'app/router', 'bootstrap-datetimepicker'], function(ko, template, module, router, datetimepicker) {
 
 	// Register the user-posts component
 	ko.components.register('user-posts', {
@@ -6,9 +6,38 @@ define(['knockout', 'text!./user.html', 'module', 'app/router'], function(ko, te
 	});
 
 	function UserViewModel(params) {
+		var self = this;
 		this.userPosts = ko.observableArray();
 		this.user = ko.observable(params.name);
 		this.tags = ko.observableArray();
+		this.displayTitle = ko.observable('Latest posts: ');
+
+		this.filterPosts = function() {
+			console.log("Filter posts clicked:");
+			console.log("From: " + $('#text-from').val());
+			console.log("To: " + $('#text-to').val());
+			var from = $('#text-from').val().match(/\d/g);
+			var to = $('#text-to').val().match(/\d/g);
+			console.log(from);
+			console.log(to);
+			if (from.length == 8 && to.length == 8) {
+				console.log(from);
+				console.log(to);
+			}
+		};
+
+		$(function () {
+			$('#date-from').datetimepicker({
+				pickTime: false
+			});
+			$('#date-to').datetimepicker({
+				pickTime: false
+			});
+			 $('#date-from').on("dp.change",function (e) {
+				$('#date-to').data("DateTimePicker")
+					.setDate($('#date-from').data("DateTimePicker").getDate());
+			});
+		});
 
 		var getUserTags = function(toStore) {
 			$.getJSON('yilpil/tags/' + params.name, function(data) {
@@ -34,6 +63,7 @@ define(['knockout', 'text!./user.html', 'module', 'app/router'], function(ko, te
 
 		getUserPosts(this.userPosts);
 		getUserTags(this.tags);
+
 	}
 	return { viewModel: UserViewModel, template: template };
 });
