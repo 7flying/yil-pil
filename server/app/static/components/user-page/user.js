366f9f43af3bd62dc7cd4335dc789160a782e389
flying <diez.f.irene@gmail.com>
2014-11-30 23:35:58 +0100

    search by date interval finished \o/

--- server/app/static/components/user-page/user.js
+++ server/app/static/components/user-page/user.js
@@ -11,18 +11,43 @@ define(['knockout', 'text!./user.html', 'module', 'app/router', 'bootstrap-datet
 		this.user = ko.observable(params.name);
 		this.tags = ko.observableArray();
 		this.displayTitle = ko.observable('Latest posts: ');
+		this.alertTitle = ko.observable("It seems that " + params.name
+			+ " hasn't post anything yet.");
 
 		this.filterPosts = function() {
-			console.log("Filter posts clicked:");
-			console.log("From: " + $('#text-from').val());
-			console.log("To: " + $('#text-to').val());
 			var from = $('#text-from').val().match(/\d/g);
+			from = from.join("");
+			// Change order
+			from = from.substring(from.length -4, from.length)
+				+ from.substring(from.length -6, from.length -4)
+				+ from.substring(0, from.length - 6);
 			var to = $('#text-to').val().match(/\d/g);
-			console.log(from);
-			console.log(to);
+			to = to.join("");
+			// Change order
+			to = to.substring(to.length -4, to.length)
+				+ to.substring(to.length -6, to.length -4)
+				+ to.substring(0, to.length - 6);
 			if (from.length == 8 && to.length == 8) {
-				console.log(from);
-				console.log(to);
+				var url = '/yilpil/search/posts/date?user=' + self.user()
+				+ "&dateini=" + from + "&dateend=" + to; 
+				$.getJSON(url, function(data) {
+					if (data.posts.length == 0) {
+						self.userPosts(null);
+						self.displayTitle("There aren't posts in that period.");
+					} else {
+						self.userPosts.removeAll();
+						self.displayTitle(data.posts.length + " posts found:");
+					}
+					while (data.posts.length > 0) {
+						var temp = data.posts.shift();
+						// Set preview
+						if (temp.contents.length < 200)
+							temp.contents = temp.contents.substring(0, temp.contents.length);
+						else
+							temp.contents = temp.contents.substring(0, 200) + " [...]";
+						self.userPosts.push(temp);
+					}
+				});
 			}
 		};
 
