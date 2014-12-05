517a2710f3a04f92182089974757dab9fbfbd749
flying <diez.f.irene@gmail.com>
2014-12-05 21:07:52 +0100

    sanitize html + highlight

--- server/app/static/components/nav-bar/nav-bar.js
+++ server/app/static/components/nav-bar/nav-bar.js
@@ -3,7 +3,7 @@ define(['knockout', 'text!./nav-bar.html'], function(ko, template) {
 	ko.bindingHandlers.executeOnEnter = {
 		init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
 			var allBindings = allBindingsAccessor();
-			$(element).keypress(function (event) {
+			$(element).keypress(function(event) {
 				var keyCode = (event.which ? event.which : event.keyCode);
 				if (keyCode === 13) {
 					allBindings.executeOnEnter.call(viewModel);
--- server/app/static/components/post-editor/post-editor.js
+++ server/app/static/components/post-editor/post-editor.js
@@ -1,11 +1,44 @@
-define(['knockout', 'text!./post-editor.html', 'marked'], function(ko, template, marked) {
+define(['knockout', 'text!./post-editor.html', 'marked', 'highlight'],
+ function(ko, template, marked, hljs) {
+
+	ko.bindingHandlers.executeOnEnter = {
+		init: function(element, valueAccessor, allBindingsAccesor, viewModel) {
+			var allBindings = allBindingsAccesor();
+			$(element).keypress(function(event) {
+				var keyCode = (event.which ? event.which : event.keyCode);
+				if (keyCode === 13) {
+					allBindings.executeOnEnter.call(viewModel);
+					return false;
+				}
+				return true;
+			});
+		}
+	};
+	marked.setOptions({
+		renderer: new marked.Renderer(),
+		gfm: true,
+		tables: true,
+		breaks: false,
+		pedantic: false,
+		sanitize: true,
+		highlight: function(code) {
+			return hljs.highlightAuto(code).value;
+		},
+		smartLists: true,
+		smartypants: false
+	});
 
 	function PostEditorViewModel(params) {
+		$(function() {
+			hljs.initHighlightingOnLoad();
+		});
 		var self = this;
 		this.mdTitle = ko.observable();
 		this.mdContents = ko.observable();
 		this.title = ko.observable();
 		this.contents = ko.observable();
+		this.tags = ko.observableArray();
+		this.tag = ko.observable();
 
 		this.mdTitle.subscribe(function(newValue) {
 			self.title(newValue);
@@ -19,6 +52,10 @@ define(['knockout', 'text!./post-editor.html', 'marked'], function(ko, template,
 		this.mdContents.subscribe(function(newValue) {
 			generateHTML(newValue);
 		});
+
+		this.addTag = function() {
+			self.tags.push(self.tag());
+		};
 	}
 
 	return { viewModel: PostEditorViewModel, template: template };
