c79df3f9b56b610499e3ba95c8179ddd24d50414
flying <diez.f.irene@gmail.com>
2014-12-09 23:40:33 +0100

    draft of mediator

---
+++ server/app/static/app/mediator.js
@@ -0,0 +1,41 @@
+define("knockout", function(ko) {
+	
+	/* Gets the posts from the given url and applies a preview. */
+	summarizePosts: function(data, toStore) {
+		while (data.posts.length > 0) {
+			var temp = data.posts.shift();
+			// Set preview
+			temp.contents = (temp.contents.length > 200 ?
+				temp.contents.substring(0, 200) + " [...]"
+				: temp.contents);
+			toStore.push(temp);
+		}
+	},
+	/* Gets the avatar from a user. */
+	getAvatar: function(toStore, username) {
+		$.getJSON('/yilpil/users' + username, function(data) {
+			toStore('http://www.gravatar.com/avatar/' + data.user.hash);
+		});
+	},
+	/* Gets the number of favourites a user has. */
+	getFavVount: function(toStore, username) {
+		$.getJSON('/yilpil/favs/' + username + '?count=true', function(data) {
+			toStore = username.count;
+		});
+	},
+	/* Gets the most popular tags from the server. */
+	getPopularTags: function(toStore) {
+		$.getJSON('/yilpil/ranking?resource=tags', function(data) {
+			while(data.tags.length > 0)
+				toStore.push(data.tags.shift());
+		});
+	},
+	/* Gets similar tags given a letter. */
+	getSimilarTags: function(toStore, letter) {
+		$.getJSON('yilpil/search/tag?letter=' + letter, function(data) {
+			while (data.tags.length > 0)
+					toStore.push(data.tags.shift());
+		});
+	}
+	
+});
